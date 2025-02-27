0a21d5c4b9f587c19e4f04cb24bc995b9836e575
mp-30 <adityabhadauriys03@gmail.com>
2025-02-27 13:46:26 +0000

    27 feb async start

---
+++ javascript_async2.js/callbacks.js
@@ -0,0 +1,16 @@
+// synchronous callbacks
+
+// function greet(name){
+//     console.log(`Hello ${name}`)
+// }
+
+// function greetVishwas(gr){
+//     const name = 'Vishwas'
+//     gr(name)
+// }
+
+// greetVishwas(greet)
+
+
+// anchronous callbacks
+
---
+++ javascript_async2.js/timeout_interval.js
@@ -0,0 +1,26 @@
+// const timeoutId = setTimeout(a, 3000, 'aditya');
+// clearTimeout(timeoutId)
+// setInterval(a, 3000, 'aditya');
+
+// function a(name){
+    
+//     console.log('first funciton is printed',name)
+// }
+
+// function b(){
+//     console.log('second funciton is printed')
+// }
+
+// b()
+
+
+
+// setTimeout(function run(){
+//     console.log('Hello')
+//     setTimeout(run, 100)
+// },100,100)
+
+
+// setInterval(function run(){
+//     console.log('Hello')
+// },100)
